9ce7b6f573cb40ba1d728225a28f9e47623b43e1
Kaustubh Bhoir <kaustubhbhoir173@gmail.com>
2026-01-05 13:21:48 +0530

    added missing photos in homePage

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -8,6 +8,10 @@ import about1 from "../images/about1.jpg"
 import about2 from "../images/about2.jpg"
 import about3 from "../images/about3.jpg"
 import step1 from "../images/step1.png"
+import applyProject from "../images/apply-project.jpeg"
+import artist from "../images/artist.jpeg"
+import gallery from "../images/gallery.jpeg"
+import postProject from "../images/post-project.jpeg"
 
 
 
@@ -26,22 +30,22 @@ const HomePage = () => {
       desc: "Both Recruiters and Artists can create an account and log in to start using CoreModeling.",
     },
     {
-      img: "/images/explore-artists.png",
+      img: artist,
       title: "Explore Artists",
       desc: "Recruiters can browse artist profiles, view their portfolios, photos, and videos.",
     },
     {
-      img: "/images/upload-projects.png",
+      img: postProject,
       title: "Upload Projects",
       desc: "Recruiters can upload exciting modeling projects for artists to discover and apply to.",
     },
     {
-      img: "/images/apply-projects.png",
+      img: applyProject,
       title: "Apply to Projects",
       desc: "Artists can explore available projects and send their applications directly through the platform.",
     },
     {
-      img: "/images/gallery.png",
+      img: gallery,
       title: "Build Your Gallery",
       desc: "Artists can upload photos and videos to their personal gallery to showcase their talent.",
     },
