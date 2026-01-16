2055079a38ef73605d6505f879cf278ecb1eceb9
Kaustubh Bhoir <kaustubhbhoir173@gmail.com>
2026-01-16 17:08:11 +0530

    remove useLocation from app.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, useLocation, Navigate } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 
 import Login from "./components/Login";
 import HomePage from "./components/Homepage";
